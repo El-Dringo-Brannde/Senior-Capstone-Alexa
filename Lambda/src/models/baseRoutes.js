b7767e841a1f78bbd95ee50f6d21a45b7daafddb
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2018-05-04 15:26:33 -0700

    Cleaned up some of the logic

--- Lambda/src/models/baseRoutes.js
+++ Lambda/src/models/baseRoutes.js
@@ -16,30 +16,6 @@ module.exports = class baseRoutes {
       })
    }
 
-   // entry point to parsing routes
-   parseRoute(intent, userID, callback) {
-      const intentName = intent.name;
-      let response = this.changeView(intent, userID, intentName, callback)
-      if (intent.slots.suggestion && intent.slots.suggestion.value == 'yes') {
-         this.checkSuggestion(intent, userID)
-            .then(resp => {
-               delete intent.slots.view
-               delete intent.slots.suggestion
-               let route = this.buildQueryString(intent.slots)
-               let sessionQuery = 'userID' + '=' + userID
-               this.sendBackReturnedData(intentName, resp, callback)
-            })
-      }
-      else {
-         delete intent.slots.view
-         delete intent.slots.suggestion
-         let route = this.buildQueryString(intent.slots)
-         let sessionQuery = 'userID' + '=' + userID
-
-         this.sendRequest(route, sessionQuery, intentName, callback)
-      }
-   }
-
    handleErr(err, callback) {
       let speechOutput = "What was that? I couldn't understand you, please try again";
       let repromptText = err;
@@ -76,31 +52,6 @@ module.exports = class baseRoutes {
       callback(this.sessionAttributes, this.buildResponse(intentName, speechOutput, repromptText, false));
    }
 
-   changeView(intents, userID, intentName, callback) {
-      if (intents.slots.view.value == 'map') {
-         let query = this.pullMapViewParams(intents, userID)
-         axios.get(this.serverURL + query)
-            .then(resp => {
-               query = query.replace('mapView', 'map')
-               axios.get(this.serverURL + query + `&group=${intents.slots.group.value}`)
-                  .then(resp => {
-                     this.sendBackReturnedData(intentName, resp, callback)
-                  })
-            });
-      } else
-         axios.get(this.serverURL + 'sales/home')
-            .then(resp => console.log(resp.data));
-   }
-
-   pullMapViewParams(intents, userID) {
-      let url = 'sales/mapView/name/'
-      url += intents.slots.location.value + '/'
-      delete intents.slots.location
-      url += 'state/' + intents.slots.state.value + '/'
-      url += 'city/' + intents.slots.city.value + '/'
-      url += `?userID=${userID}`
-      return url
-   }
 
    sendRequest(route, sessionQuery, intentName, callback) {
       console.log(this.serverURL + route.toLowerCase() + sessionQuery)
